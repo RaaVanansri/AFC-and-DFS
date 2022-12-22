e5c2df5ce9eb9f8b7174bb5ba9781065fc61b31e
RaaVananSri <vigneshseetharam@outlook.com>
2022-12-22 11:07:00 +0530

    TDWRfixed

--- dfs.py
+++ dfs.py
@@ -3,21 +3,16 @@ channels = {36:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,
     40:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     44:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     48:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
-    38:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
-    46:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
-    42:{"Frequency":"80Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     149:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     153:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     157:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     161:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     165:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
-    151:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
-    159:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
-    155:{"Frequency":"80Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     52:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     56:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     60:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     64:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    144:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     100:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     104:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     108:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
@@ -27,8 +22,10 @@ channels = {36:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,
     124:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
     128:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
     132:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    136:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     140:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
-    144:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    38:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    46:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     54:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     62:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     102:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
@@ -37,6 +34,10 @@ channels = {36:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,
     126:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     134:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     142:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    151:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    159:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    155:{"Frequency":"80Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    42:{"Frequency":"80Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
     58:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     106:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
     122:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
@@ -48,11 +49,21 @@ channels = {36:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,
 def randint(chn,country):
     tdwr = {"India":True,"Russia":True,"UK":False,"US":True,"Japan":False,"China":False}
 
+    def avail():
+        ach = ''
+        for i in channels:
+            if channels[i]["Availability"] == 1 and channels[i]["TDWR"] == False and tdwr.get(country) == True:
+                ach += f"\nChannel {i} with {channels[i]['Frequency']}"
+            elif tdwr.get(country) == False and channels[i]["Availability"] == 1:
+                ach += f"\nChannel {i} with {channels[i]['Frequency']}"
+            
+        return ach
+
     if channels[chn]["TDWR"] and tdwr.get(country) == True:
-        availablech = ''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 and channels[i]["TDWR"] == False and tdwr.get(country) == True else '' for i in channels)
+        availablech = avail() #''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 and channels[i]["TDWR"] == False and tdwr.get(country) == True else '' for i in channels)
         return f"Can\'t transmit data in the Selected channel No.{chn}.It is preoccupied by TDWR in selected country {country}\nThese Channels are available to transmit data:{availablech}"
     elif channels[chn]["DFS"] and channels[chn]["Availability"] == 0:
-        availablech = ''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 else '' for i in channels)
+        availablech = avail() #''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 and channels[i]["TDWR"] == False else '' for i in channels)
         return f"Selected Channel No.{chn} is not available due to occupied by other services.\nThese Channels are available to transmit data:{availablech}"
     elif channels[chn]["Availability"] == 0:
         availablech = ''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 else '' for i in channels)
