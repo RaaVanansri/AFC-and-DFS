2e21bf461f502a89a8455f19e166bcad866f3183
RaaVananSri <vigneshseetharam@outlook.com>
2022-12-22 10:40:36 +0530

    refactored

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import dfs
 import os
 
 tdwr = ["India","Russia","UK","US","Japan","China"]
+channelno = [52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,144,36,40,44,48,149,153,157,161,165,54,62,102,110,118,126,134,142,38,46,151,159,58,106,122,138,42,155,114,50]
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    channelno = [52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,144,36,40,44,48,149,153,157,161,165,54,62,102,110,118,126,134,142,38,46,151,159,58,106,122,138,42,155,114,50]
     channelno.sort()
     if request.method == 'POST':
         ch = int(request.form.get('channel'))
         countrytd = request.form.get('country')
         ch = dfs.randint(ch,countrytd)
+        ch = ch.replace('\n','<br>')
         return render_template('home.html',out=ch,channel=channelno,country=tdwr)
     return render_template('home.html',out='',channel=channelno,country=tdwr)
 
--- dfs.py
+++ dfs.py
@@ -1,28 +1,61 @@
 import random
-
+channels = {36:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    40:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    44:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    48:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    38:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    46:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    42:{"Frequency":"80Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    149:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    153:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    157:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    161:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    165:{"Frequency":"20Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    151:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    159:{"Frequency":"40Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    155:{"Frequency":"80Mhz","DFS":False,"Availability":random.randint(0,1),"TDWR":False},
+    52:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    56:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    60:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    64:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    100:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    104:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    108:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    112:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    116:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    120:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
+    124:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
+    128:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
+    132:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    140:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    144:{"Frequency":"20Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    54:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    62:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    102:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    110:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    118:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
+    126:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    134:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    142:{"Frequency":"40Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    58:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    106:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    122:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
+    138:{"Frequency":"80Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    50:{"Frequency":"160Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":False},
+    114:{"Frequency":"160Mhz","DFS":True,"Availability":random.randint(0,1),"TDWR":True},
+    }
+    
 def randint(chn,country):
-    channels = [["20Mhz DFS",52,56,60,64,100,104,108,112,116,120,124,128,132,136,140,144],
-    ["20Mhz Non-DFS",36,40,44,48,149,153,157,161,165],
-    ["40Mhz DFS",54,62,102,110,118,126,134,142],
-    ["40Mhz Non-DFS",38,46,151,159],
-    ["80Mhz DFS",58,106,122,138],
-    ["80Mhz Non-DFS",42,155],
-    ["160Mhz DFS",114],
-    ["160Mhz Non-DFS",50]]
-
-    PartialChannel = [50,114]
-
-    tdwrch = [120,124,128,118,126,122,114]
     tdwr = {"India":True,"Russia":True,"UK":False,"US":True,"Japan":False,"China":False}
 
-    for i in range(len(channels)):
-        if chn in tdwrch and tdwr.get(country) == True:
-            return f"TDWR is occuiped this channel in selected country {country}.Can\'t transmit data in the selected Channel No.{chn}"
-        elif channels[i][0].split(' ')[1] in "DFS" and chn in channels[i]:
-            k = random.randint(0,1)
-            if k == 1 :
-                return f"Selected Channel No.{chn} is available to transmit data"
-            else:
-                return f"Selected Channel No.{chn} is not available to transmit data"
-        elif channels[i][0].split(' ')[1] in "Non-DFS" and chn in channels[i]:
-            return f"Selected Channel No.{chn} is available to transmit data"
+    if channels[chn]["TDWR"] and tdwr.get(country) == True:
+        availablech = ''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 and channels[i]["TDWR"] == False and tdwr.get(country) == True else '' for i in channels)
+        return f"Can\'t transmit data in the Selected channel No.{chn}.It is preoccupied by TDWR in selected country {country}\nThese Channels are available to transmit data:{availablech}"
+    elif channels[chn]["DFS"] and channels[chn]["Availability"] == 0:
+        availablech = ''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 else '' for i in channels)
+        return f"Selected Channel No.{chn} is not available due to occupied by other services.\nThese Channels are available to transmit data:{availablech}"
+    elif channels[chn]["Availability"] == 0:
+        availablech = ''.join(f"\nChannel {i} with {channels[i]['Frequency']}" if channels[i]["Availability"] == 1 else '' for i in channels)
+        return f"Selected Channel No.{chn} is interfered by too many networks.\nThese are suggested Channels to transmit data:{availablech}"
+    else:
+        return f"Selected Channel No.{chn} is available to transmitdata"
